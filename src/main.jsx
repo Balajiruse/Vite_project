18a702068edb113bdfd1e671d29fad5eb76ecd50
Balajiruse <balaji.ruse@gmail.com>
2023-11-30 19:20:06 +0530

    context

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-//import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+// import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import AppContext from "./Contex.jsx/AppContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+    <AppContext>
+      <App />
+    </AppContext>
+  </BrowserRouter>
+);
